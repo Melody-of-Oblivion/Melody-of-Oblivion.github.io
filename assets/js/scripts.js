0b498abd5a9724b51aad17dc81c26a8fd92d659d
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2020-03-14 16:47:14 +0800

    Update
    build ver1.5.0

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,3 @@
-// styles toggle
-$(document).ready(function(){
-    var stylechoice = $.cookie('stylecheck');
-    if (stylechoice==1) styleToggle(1);
-    if (stylechoice==2) styleToggle(2);
-});
-function btn1() {
-    $.cookie('stylecheck', '1', { expires: 7, path: '/' });
-    styleToggle(1);
-}
-function btn2() {
-    $.cookie('stylecheck', '2', { expires: 7, path: '/' });
-    styleToggle(2);
-}
-function styleToggle(sort) {
-    var css=document.getElementById("style");
-    if (sort==1) css.setAttribute("href","/assets/css/main-1.css");
-    if (sort==2) css.setAttribute("href","/assets/css/main-2.css");
-}
-
 // to-top button
 $(document).ready(function() {
     $(function() {
