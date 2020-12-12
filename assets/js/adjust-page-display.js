e73dcc442990bb2eb759dae0b33f3c560398bae6
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2020-12-12 12:10:57 +0800

    Update display

--- assets/js/adjust-page-display.js
+++
@@ -1,9 +0,0 @@
-// 页面过小时调整大小
-$(document).ready(function(){
-    var a = ($(window).height());
-    var b = ($(document.body).height());
-    if (a > b) {
-        var height=document.getElementById("window");
-        height.setAttribute("href","/assets/css/window.css");
-    }
-});
