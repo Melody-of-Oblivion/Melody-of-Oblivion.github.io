2a82f4610f4c3d21be08a3ef0ef69c297d2196b4
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2021-07-08 16:59:45 +0800

    Update

--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -1,35 +1,3 @@
----
-title: JQ写网页浏览进度条
-editdate: 2021-07-08
----
-
-> 仅供参考。
-
-html部分：
-
-```html
-<div class="progressbar"></div>
-```
-
-css部分：
-
-```css
-// 顶部进度条，根据情况修改
-.progressbar {
-    top: 0;
-    left: 0;
-    position: fixed;
-    z-index: 5;
-    width: 0;
-    height: 5px;
-    background-color: #777;
-}
-```
-
-js部分：
-
-```javascript
-// 用到JQ
 function progbar(){
     var st = $(window).scrollTop(); // 距顶部距离
     var cr = $(window).height(); // 窗口高度
@@ -44,5 +12,3 @@ function progbar(){
 
 $(document).ready(function(){progbar();}) // 网页加载时
 $(document).scroll(function(){progbar();}) // 网页滚动时
-```
-
